f38d0c8e3b9e7fa4ac4f265f776b8168085ec700
Alejandro Nanez <git@alejandro.dev>
2022-09-24 14:45:19 -0500

    delete dead code: greet fn

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-
-    format!("Hello {}!", &name)
-}
-
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
