916be138b5802f5d4ce32f84be0a621fb8032cfb
Alejandro Nanez <git@alejandro.dev>
2022-09-27 14:32:38 -0500

    passing connection to run. it fails because HttpServer expects PgConnection to be cloneable

--- src/startup.rs
+++ src/startup.rs
@@ -2,14 +2,16 @@ use std::net::TcpListener;
 
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgConnection;
 
 use crate::routes::{health_check, subscribe};
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(connection)
     })
     .listen(listener)?
     .run();
