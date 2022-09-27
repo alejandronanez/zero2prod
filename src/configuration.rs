518f635bc36cc62c5fe08d737b3854aba8458de6
Alejandro Nanez <git@alejandro.dev>
2022-09-27 10:46:26 -0500

    read and serialize a configuration file

--- src/configuration.rs
+++ src/configuration.rs
@@ -12,3 +12,17 @@ pub struct DatabaseSettings {
     pub host: String,
     pub database_name: String,
 }
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    // initialize the configuration reader
+    let settings = config::Config::builder()
+        // add configuration values from a file named `configuration.yaml`
+        .add_source(config::File::new(
+            "configuration.yaml",
+            config::FileFormat::Yaml,
+        ))
+        .build()?;
+
+    // try to convert the configuration values into the `Settings` type declared above
+    settings.try_deserialize::<Settings>()
+}
