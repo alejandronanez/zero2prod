25d8ec40eafe8ed12e90d0122dadaae5eee3abaf
Alejandro Nanez <git@alejandro.dev>
2022-09-27 10:54:36 -0500

    reading the configuration from a configuration yaml file

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-
-    let port = listener.local_addr().unwrap().port();
-
-    println!("listening on port {}", port);
+    // Panic if we can't find the config
+    let configuration = get_configuration().expect("Failed to read configuration!");
+    // Use the port from our settings file!
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
 
     run(listener)?.await
 }
