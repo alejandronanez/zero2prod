604abfe0fbb2d05cf2e9895ba4a2556de2c53c5e
Alejandro Nanez <git@alejandro.dev>
2022-09-27 09:34:17 -0500

    make sure docker is running before run sqlx

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -21,6 +21,15 @@ docker run \
   postgres -N 1000
   # ^ Increased maximum number of connections for testing purposes
 
+  # Keep pinging Postgres until it's ready to accept commands
+  export PGPASSWORD="${DB_PASSWORD}"
+  until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+    >&2 echo "Postgres is still unavailable - sleeping"
+    sleep 1
+  done
+  
+  >&2 echo "Postgres is up and running on port ${DB_PORT}!"
+
   DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
   export DATABASE_URL
   sqlx database create
